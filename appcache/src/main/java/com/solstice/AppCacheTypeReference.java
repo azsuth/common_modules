77761cc0ef6b3b2051b401ab3a3a71a8e907f105
Andrew Sutherland <azsuth@gmail.com>
2015-05-08 08:49:39 -0500

    appcache and volleyJacksonRequest

---
+++ appcache/src/main/java/com/solstice/AppCacheTypeReference.java
@@ -0,0 +1,32 @@
+package com.solstice;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * Created by andrewsutherland on 4/14/15.
+ */
+public abstract class AppCacheTypeReference<T> {
+    private final Type type;
+    private volatile Constructor<?> constructor;
+
+    protected AppCacheTypeReference() {
+        Type superclass = getClass().getGenericSuperclass();
+        if (superclass instanceof Class) {
+            throw new RuntimeException("Missing type parameter");
+        }
+        type = ((ParameterizedType) superclass).getActualTypeArguments()[0];
+    }
+
+    final public T newInstance() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        if (constructor == null) {
+            Class<?> rawType = type instanceof Class<?>
+                    ? (Class<?>) type
+                    : (Class<?>) ((ParameterizedType) type).getRawType();
+            constructor = rawType.getConstructor();
+        }
+        return (T) constructor.newInstance();
+    }
+}
